bfc62e2a42debb9a5de1f92e400fa115bc5dcbe1
mishanwong <32437766+mishanwong@users.noreply.github.com>
2020-04-29 17:35:47 -0700

    Connected to database

--- backend.py
+++ backend.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+#from flask_mysqldb import MySQL
+#import MySQLdb.cursors
 import mysql.connector
 
 app = Flask(__name__)
@@ -8,16 +10,20 @@ app.secret_key = 'dhaulagiri'
 dbconfig = {    'host': '127.0.0.1',
                 'user': 'mimi-db',
                 'password': 'digital-nomad',
-                'database': 'mountaineruDB' }
+                'database': 'mountaineruDB',}
+
+#Database connection details
+#app.config['MYSQL_HOST'] = '127.0.0.1'
+#app.config['MYSQL_USER'] = 'mimi-db'
+#app.config['MYSQL_PASSWORD'] = 'digital-nomad'
+#app.config['MYSQL_DB'] = 'mountaineruDB'
+
+#Initialize MySQL
+#mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = '''select * from MountainTour''' #Insert SQL query here
-    cursor.execute(_SQL)
-    results = cursor.fetchall()
-    return results #redirect(url_for('homepage'))
+    return redirect(url_for('homepage'))
 
 @app.route('/home', methods = ['POST', 'GET'])
 def homepage():
@@ -27,16 +33,29 @@ def homepage():
 def about():
     return render_template('about.html', the_title="About Mountaineru")
 
-"""
 @app.route('/result', methods = ['POST', 'GET'])
 def do_search() -> 'html':
-    searchquery = request.form['searchquery']
-    datefrom = request.form['datefrom']
-    dateto = request.form['dateto']
-    difficulty = request.form.getlist('difficulty')
-    title = 'Here are what you searched for: '
-    return render_template('result.html', the_title=title, the_searchquery=searchquery, the_datefrom=datefrom, the_dateto=dateto, the_difficulty=list(difficulty),)
-"""
+    if request.method == 'POST':
+        #searchquery = request.form['searchquery']
+        #datefrom = request.form['datefrom']
+        #dateto = request.form['dateto']
+        #difficulty = request.form.getlist('difficulty') 
+        
+        conn = mysql.connector.connect(**dbconfig)
+        cursor = conn.cursor()
+        _SQL = """SELECT * FROM MountainTour"""
+        cursor.execute(_SQL)
+        searchresult = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        #cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        #cursor.execute('''SELECT * FROM MountainTour''')
+        # Fetch all record and return result
+        #searchresult = cursor.fetchall()
+
+    return render_template('result.html', output_data=searchresult)
 
 if __name__ == '__main__':
     app.run(debug=True)
