9abc8dc38ac0ecf74e78b496859dc709d9cebbce
mishanwong <32437766+mishanwong@users.noreply.github.com>
2020-05-19 16:59:45 -0700

    add environment variables

--- application.py
+++ application.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
+import os
 
 application = app = Flask(__name__)
 
 app.secret_key = 'dhaulagiri'
-
-dbconfig = {    'host': 'aa1c3p2qmazyyc1.cpksfpeuyyky.us-west-2.rds.amazonaws.com',
-                'user': 'mimineruDB',
-                'password': 'digital-nomad',
-                'database': 'MountaineruDB',
-                'port': 3306,}
 
 @app.route('/')
 def index():
@@ -31,20 +26,23 @@ def do_search() -> 'html':
         year = request.form['year']
         difficulty = request.form['difficulty']
         
-        conn = mysql.connector.connect(**dbconfig)
+        
+        conn = mysql.connector.connect(host='127.0.0.1', 
+                                    user=os.environ['DB_USER'],
+                                    password=os.environ['DB_PASSWORD'],
+                                    database='mountaineruDB')
         cursor = conn.cursor()
         _SQL = """SELECT * FROM MountainTour
-            WHERE TourName LIKE %s
-            AND month(StartDate) = %s
-            AND year(StartDate) = %s
-            AND Difficulty = %s """
+                WHERE TourName LIKE %s
+                AND month(StartDate) = %s
+                AND year(StartDate) = %s
+                AND Difficulty = %s """
         args = ('%' + searchquery + '%'), month, year, difficulty
         cursor.execute(_SQL, args)
         searchresult = cursor.fetchall()
-
         cursor.close()
         conn.close()
-
+        
     return render_template('result.html', output_data=searchresult)
 
 if __name__ == '__main__':
